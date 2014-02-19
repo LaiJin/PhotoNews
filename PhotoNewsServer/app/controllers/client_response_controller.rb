f471e28a0f1035d21f717a7e7122d68ebc35d1f9
LaiJin <LuaJin312@gmail.com>
2014-02-19 15:49:53 +0800

    新的数据显示在前面

--- PhotoNewsServer/app/controllers/client_response_controller.rb
+++ PhotoNewsServer/app/controllers/client_response_controller.rb
@@ -2,6 +2,7 @@ class ClientResponseController < ApplicationController
 
   def respond_client_request
     @image_news = ImageNews.all
+    @image_news =  @image_news.reverse
     respond_to do |format|
       format.json {render :json =>  @image_news.to_json}
     end
