d9a170ceb232a9adf874374a619a71403e5be2ed
LaiJin <LuaJin312@gmail.com>
2014-03-02 17:10:11 +0800

    has_secure_password

--- PhotoNewsServer/app/models/user.rb
+++ PhotoNewsServer/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   before_create { generate_token(:token) }
 
+  has_secure_password
+
   def generate_token(column)
     begin
       self[column] = SecureRandom.urlsafe_base64
