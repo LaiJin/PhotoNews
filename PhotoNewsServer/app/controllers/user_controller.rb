293892686c52dbd69207e2c7d16d6db8b42d86ec
LaiJin <LuaJin312@gmail.com>
2014-03-02 18:20:08 +0800

    add logout method

--- PhotoNewsServer/app/controllers/user_controller.rb
+++ PhotoNewsServer/app/controllers/user_controller.rb
@@ -5,13 +5,19 @@ class UserController < ApplicationController
   end
 
   def create_login_session
-    user = User.find_by_name(params[:name])
-    if user && user.authenticate(params[:password])
+    user = User.find_by_name(params[:user][:name])
+
+    if user && user.authenticate(params[:user][:password])
       cookies.permanent[:token] = user.token
       redirect_to :image_show_view
       return
     end
     flash[:login_error] = '用户名或密码错误'
+    redirect_to :login
+  end
+
+  def logout
+    cookies.delete(:token)
     redirect_to :login
   end
 
--- PhotoNewsServer/config/routes.rb
+++ PhotoNewsServer/config/routes.rb
@@ -11,5 +11,6 @@ PhotoNewsServer::Application.routes.draw do
   get  'image_upload/show_detailed_image_news_view' => 'image_upload#show_detailed_image_news_view', :as => 'show_detailed_image_news_view'
   get  'image_news_data' => 'client_response#respond_client_request'
   delete 'image_upload/image_show_view' => 'image_upload#delete_image_news', :as => 'delete_image_news'
+  delete 'logout' => 'user#logout', :as => 'logout'
 
 end
