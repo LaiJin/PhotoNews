166f16409aca90aadbadf5b81f693cb92f61990d
LaiJin <LuaJin312@gmail.com>
2014-03-02 17:06:16 +0800

    user login set token value

--- PhotoNewsServer/app/controllers/user_controller.rb
+++ PhotoNewsServer/app/controllers/user_controller.rb
@@ -5,7 +5,14 @@ class UserController < ApplicationController
   end
 
   def create_login_session
-      redirect_to :login
+    user = User.find_by_name(params[:name])
+    if user && user.authenticate(params[:password])
+      cookies.permanent[:token] = user.token
+      redirect_to :image_show_view
+      return
+    end
+    flash[:login_error] = '用户名或密码错误'
+    redirect_to :login
   end
 
 end
--- PhotoNewsServer/db/schema.rb
+++ PhotoNewsServer/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212075705) do
+ActiveRecord::Schema.define(version: 20140302085034) do
 
   create_table "image_news", force: true do |t|
     t.text     "content"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20140212075705) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.string   "token"
+    t.boolean  "isAdmin",         default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
