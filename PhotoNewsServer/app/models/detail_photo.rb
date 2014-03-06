b682410926729e0b92d84ed3fbc01c1f5e981743
LaiJin <LuaJin312@gmail.com>
2014-03-06 13:56:56 +0800

    configure model

--- PhotoNewsServer/app/models/detail_photo.rb
+++ PhotoNewsServer/app/models/detail_photo.rb
@@ -1,2 +1,14 @@
 class DetailPhoto < ActiveRecord::Base
+
+  belongs_to :title_photo
+
+  has_attached_file :photo,
+                    :styles => { :small => '150x150>'},
+                    :url  => '/detailPhoto/:style/:basename.:extension',
+                    :path => ':rails_root/public/detailPhoto/:style/:basename.:extension'
+  validates_attachment_presence :image
+  validates_attachment_size :image, :less_than => 5.megabytes
+  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/png)
+  validates_presence_of :content, :title_photo_id
+
 end
--- PhotoNewsServer/app/models/title_photo.rb
+++ PhotoNewsServer/app/models/title_photo.rb
@@ -1,2 +1,14 @@
 class TitlePhoto < ActiveRecord::Base
+
+  has_many :detail_photos
+
+  has_attached_file :photo,
+                    :styles => { :small => '150x150>'},
+                    :url  => '/titlePhoto/:style/:basename.:extension',
+                    :path => ':rails_root/public/titlePhoto/:style/:basename.:extension'
+  validates_attachment_presence :image
+  validates_attachment_size :image, :less_than => 5.megabytes
+  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/png)
+  validates_presence_of :title, :synopsis
+
 end
