0ba565f2e0640dacf6dbee818f123e8363cfdf86
LaiJin <LuaJin312@gmail.com>
2014-03-06 14:19:39 +0800

    modify model

--- PhotoNewsServer/app/models/detail_photo.rb
+++ PhotoNewsServer/app/models/detail_photo.rb
@@ -6,9 +6,9 @@ class DetailPhoto < ActiveRecord::Base
                     :styles => { :small => '150x150>'},
                     :url  => '/detailPhoto/:style/:basename.:extension',
                     :path => ':rails_root/public/detailPhoto/:style/:basename.:extension'
-  validates_attachment_presence :image
-  validates_attachment_size :image, :less_than => 5.megabytes
-  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/png)
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => %w(image/jpeg image/png)
   validates_presence_of :content, :title_photo_id
 
 end
--- PhotoNewsServer/app/models/title_photo.rb
+++ PhotoNewsServer/app/models/title_photo.rb
@@ -6,9 +6,9 @@ class TitlePhoto < ActiveRecord::Base
                     :styles => { :small => '150x150>'},
                     :url  => '/titlePhoto/:style/:basename.:extension',
                     :path => ':rails_root/public/titlePhoto/:style/:basename.:extension'
-  validates_attachment_presence :image
-  validates_attachment_size :image, :less_than => 5.megabytes
-  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/png)
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => %w(image/jpeg image/png)
   validates_presence_of :title, :synopsis
 
 end
